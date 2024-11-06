31f65d77061978fe930242059ed67c61528b2633
Josevan Oliveira <josivantarcio@msn.com>
2024-11-06 08:47:01 -0300

    include paste #03

---
+++ aula-3/aula/comentarios.py
@@ -0,0 +1,9 @@
+#comentarios de uma linha
+print("comentario de uma linha");
+"""
+comentarios de múltiplas linhas
+"""
+nome = input ("qual seu nome?")
+print(f"Olá, {nome}. Como vai voce?")
+
+# Definindo variáveis	
---
+++ aula-3/aula/primeiro_programa.py
@@ -0,0 +1,5 @@
+print("Hello, world!")
+
+nome = input("qual seu nome? ")
+
+print("Olá", nome, "!")
